fb1fd7a36333fe6a1d290dd8bf8592150eed7ea8
b1ondie96 <103701664+b1ondie96@users.noreply.github.com>
2023-06-06 19:28:52 +0200

    init

---
+++ src/index.test.ts
@@ -0,0 +1,77 @@
+/**
+ * @jest-environment jsdom
+ */
+import { renderHook, act } from "@testing-library/react";
+import useRemainingTime from ".";
+jest.useFakeTimers();
+describe("initial state", () => {
+  it("should set secondsRemaining", () => {
+    const { result } = renderHook(() => useRemainingTime(10, false));
+    expect(result.current.secondsRemaining).toBe(10);
+  });
+  it("should set timeString", () => {
+    const { result } = renderHook(() => useRemainingTime(-10, false));
+    expect(result.current.timeString).toBe("00:00:10");
+  });
+  it("should set isPaused", () => {
+    const { result } = renderHook(() => useRemainingTime(-10, true));
+    expect(result.current.isPaused).toBe(true);
+  });
+});
+describe("time string", () => {
+  it("timeString should be 00:00:00", () => {
+    const { result } = renderHook(() => useRemainingTime(0, false));
+    expect(result.current.timeString).toBe("00:00:00");
+  });
+  it("timeString should be 00:06:00", () => {
+    const { result } = renderHook(() => useRemainingTime(360, false));
+    expect(result.current.timeString).toBe("00:06:00");
+  });
+  it("timeString should be 00:05:59", () => {
+    const { result } = renderHook(() => useRemainingTime(-360, false));
+    expect(result.current.timeString).toBe("00:06:00");
+    act(() => jest.advanceTimersByTime(1000));
+    expect(result.current.timeString).toBe("00:05:59");
+  });
+});
+describe("timer", () => {
+  it("timer should tick every second", () => {
+    const { result } = renderHook(() => useRemainingTime(3, false));
+
+    act(() => jest.advanceTimersByTime(1000));
+    expect(result.current.secondsRemaining).toBe(2);
+    expect(result.current.timeString).toBe("00:00:02");
+    act(() => jest.advanceTimersByTime(1000));
+    expect(result.current.secondsRemaining).toBe(1);
+    act(() => jest.advanceTimersByTime(1000));
+    expect(result.current.secondsRemaining).toBe(0);
+    expect(result.current.isPaused).toBe(true);
+  });
+  it("timer should be paused on 0 seconds remaining", () => {
+    const { result } = renderHook(() => useRemainingTime(0, false));
+    expect(result.current.isPaused).toBe(true);
+  });
+  it("should call onCompleted on 0 seconds", () => {
+    const fn = jest.fn();
+    const { result } = renderHook(() => useRemainingTime(0, false, fn));
+    expect(fn).toHaveBeenCalled();
+  });
+  it("should reset", () => {
+    const { result } = renderHook(() => useRemainingTime(5, false));
+    expect(result.current.secondsRemaining).toBe(5);
+    act(() => jest.advanceTimersByTime(1000));
+    expect(result.current.secondsRemaining).toBe(4);
+    act(() => result.current.resetTimer());
+    expect(result.current.secondsRemaining).toBe(5);
+  });
+  it("should pause", () => {
+    const { result } = renderHook(() => useRemainingTime(5, false));
+    expect(result.current.secondsRemaining).toBe(5);
+    act(() => jest.advanceTimersByTime(1000));
+    expect(result.current.secondsRemaining).toBe(4);
+    act(() => result.current.setIsPaused(true));
+    act(() => jest.advanceTimersByTime(1000));
+    act(() => jest.advanceTimersByTime(1000));
+    expect(result.current.secondsRemaining).toBe(4);
+  });
+});
---
+++ src/index.ts
@@ -0,0 +1,86 @@
+import React from "react";
+export interface UseTimerState {
+  /** Time remaining as a hh:mm:ss string. */
+  timeString: string;
+  /** Time remaining in seconds. */
+  secondsRemaining: number;
+  /** Whether the timer is paused. */
+  isPaused: boolean;
+
+  /** Whether the timer should count down. */
+  setIsPaused(value: boolean): void;
+  /**
+   * Reset the timer.
+   *
+   * @param pause Whether the timer should freeze after resetting.
+   */
+  resetTimer(pause?: boolean): void;
+}
+/**
+ * @param initialSeconds Start of timer.
+ * @param initialPaused Initial state, if true, the timer will not tick.
+ * @param onCompleted Callback when timer hits 0 seconds remaining.
+ */
+export function useRemainingTime(
+  initialSeconds: number,
+  initialPaused: boolean,
+  onCompleted?: () => void
+): UseTimerState {
+  const [secondsRemaining, setSecondsRemaining] = React.useState(
+    Math.abs(initialSeconds)
+  );
+  const [timeString, setTimeString] = React.useState(
+    formatTime(Math.abs(initialSeconds))
+  );
+  const [isPaused, setIsPaused] = React.useState(initialPaused);
+
+  React.useEffect(() => {
+    if (secondsRemaining > 0 && !isPaused) {
+      const timer = setTimeout(() => {
+        const nextTick = secondsRemaining - 1;
+        setTimeString(formatTime(nextTick));
+        setSecondsRemaining(nextTick);
+      }, 1000);
+      return () => clearTimeout(timer);
+    } else if (secondsRemaining === 0 && !isPaused) {
+      setIsPaused(true);
+      if (onCompleted) onCompleted();
+    }
+  }, [isPaused, secondsRemaining]);
+
+  React.useEffect(() => {
+    setTimeString(formatTime(Math.abs(initialSeconds)));
+    setSecondsRemaining(Math.abs(initialSeconds));
+  }, [initialSeconds]);
+
+  const resetTimer = React.useCallback(
+    (pause: boolean = false) => {
+      setSecondsRemaining(Math.abs(initialSeconds));
+      setTimeString(formatTime(Math.abs(initialSeconds)));
+      setIsPaused(pause);
+    },
+    [initialSeconds]
+  );
+
+  return {
+    timeString,
+    secondsRemaining,
+    isPaused,
+    setSecondsRemaining,
+    setIsPaused,
+    resetTimer,
+  } as UseTimerState;
+}
+
+function formatTime(time: number) {
+  const secondsRemaining = Math.abs(time);
+  if (secondsRemaining === 0) return "00:00:00";
+  const hours = Math.floor(secondsRemaining / (60 * 60));
+  const minutes = Math.floor((secondsRemaining % (60 * 60)) / 60);
+  const seconds = Math.floor(secondsRemaining % 60);
+
+  return `${hours.toString().padStart(2, "0")}:${minutes
+    .toString()
+    .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+}
+export default useRemainingTime;
